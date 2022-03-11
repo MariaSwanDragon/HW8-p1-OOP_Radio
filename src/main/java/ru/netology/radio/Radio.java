de7c8d3c8fb1ba2b1d08df2a45002f23ff1a9ff1
Mariia <lebedevamv1@gmail.com>
2022-03-11 15:58:13 +0300

    start

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,84 @@
+package ru.netology.radio;
+
+
+public class Radio {
+    private int currentChannel;
+    private int currentVolume;
+
+
+    public Radio() {
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public void setToMaxChannel() {
+        currentChannel = 9;
+    }
+
+    public void setToMinChannel() {
+        currentChannel = 0;
+    }
+
+    public void nextChannel() {
+        if (currentChannel < 9) {
+            currentChannel = currentChannel + 1;
+            return;
+        }
+        this.currentChannel = 0;
+    }
+
+    public void prevChannel() {
+        if (currentChannel > 1) {
+            currentChannel = currentChannel - 1;
+        }
+        if (currentChannel == 0) {
+            this.currentChannel = 9;
+        }
+    }
+
+    public void setCurrentChannel(int CurrentChannel) {
+        if (CurrentChannel > 9) {
+            return;
+        }
+        if (CurrentChannel < 0) {
+            return;
+        }
+        this.currentChannel = CurrentChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setToMaxVolume() {
+        currentVolume = 10;
+    }
+
+    public void setToMinVolume() {
+        currentVolume = 0;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void setCurrentVolume(int CurrentVolume) {
+        if (CurrentVolume > 10) {
+            return;
+        }
+        if (CurrentVolume < 0) {
+            return;
+        }
+        this.currentVolume = CurrentVolume;
+    }
+}
